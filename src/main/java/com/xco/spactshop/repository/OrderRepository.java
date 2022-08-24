3c7574be40a8f72c5f8ef1f045b83c46bd8967d3
Manikarthik Chandankeri <mc21407@corp.ojas-it.com>
2022-08-24 17:32:04 +0530

    spring_mvc_evommerce

---
+++ src/main/java/com/xco/spactshop/repository/OrderRepository.java
@@ -0,0 +1,14 @@
+package com.xco.spactshop.repository;
+
+
+import com.xco.spactshop.model.Order;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
+public interface OrderRepository extends JpaRepository<Order, String> {
+
+    List<Order> findAllByUser(String user_id);
+}
